c153ada78b34a6f1ee88ad9c8fe0f32c38941529
OriginalYellow <michael.robert.joseph1@gmail.com>
2020-04-09 20:32:22 -0400

    add auth; add auth to vue-apollo

--- www/nuxt.config.js
+++ www/nuxt.config.js
@@ -46,7 +46,8 @@ export default {
     '@nuxtjs/pwa',
     // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
-    '@nuxtjs/apollo'
+    '@nuxtjs/apollo',
+    '@nuxtjs/auth'
   ],
   /*
   ** Axios module configuration
@@ -64,78 +65,40 @@ export default {
       dark: false
     }
   },
+  auth: {
+    redirect: {
+      login: '/',
+      // logout: '/',
+      callback: '/',
+      home: '/'
+    },
+    strategies: {
+      auth0: {
+        domain: 'tekken-notes-production.auth0.com',
+        client_id: 'oxvawDM5qVP7PR85CnPp75Ea9mK11wdh'
+      }
+    }
+  },
   apollo: {
-    tokenName: '', // optional, default: apollo-token
-    // cookieAttributes: {
-    //   /**
-    //     * Define when the cookie will be removed. Value can be a Number
-    //     * which will be interpreted as days from time of creation or a
-    //     * Date instance. If omitted, the cookie becomes a session cookie.
-    //     */
-    //   expires: 7, // optional, default: 7 (days)
-
-    //   /**
-    //     * Define the path where the cookie is available. Defaults to '/'
-    //     */
-    //   path: '/', // optional
-    //   /**
-    //     * Define the domain where the cookie is available. Defaults to
-    //     * the domain of the page where the cookie was created.
-    //     */
-    //   domain: 'example.com', // optional
-
-    //   /**
-    //     * A Boolean indicating if the cookie transmission requires a
-    //     * secure protocol (https). Defaults to false.
-    //     */
-    //   secure: false
-    // },
-    includeNodeModules: true, // optional, default: false (this includes graphql-tag for node_modules folder)
-    authenticationType: 'Basic', // optional, default: 'Bearer'
-    // (Optional) Default 'apollo' definition
+    tokenName: 'auth._token.auth0',
+    includeNodeModules: true,
+    authenticationType: '',
     defaultOptions: {
-      // See 'apollo' definition
-      // For example: default query options
       $query: {
         loadingKey: 'loading',
-        fetchPolicy: 'cache-and-network'
+        fetchPolicy: 'cache-first'
       }
     },
-    // optional
-    // watchLoading: '~/plugins/apollo-watch-loading-handler.js',
-    // optional
     errorHandler: '~/plugins/apollo-error-handler.js',
-    // required
     clientConfigs: {
       default: {
-        // required
         httpEndpoint: process.env.HASURA_ENDPOINT || 'http://localhost:8080/v1/graphql',
-        // optional
-        // override HTTP endpoint in browser only
-        // browserHttpEndpoint: '/graphql',
-        // optional
-        // See https://www.apollographql.com/docs/link/links/http.html#options
         httpLinkOptions: {
           credentials: 'same-origin'
         },
-        // You can use `wss` for secure connection (recommended in production)
-        // Use `null` to disable subscriptions
-        // wsEndpoint: 'ws://localhost:4000', // optional
-        // LocalStorage token
-        tokenName: '', // optional
-        // Enable Automatic Query persisting with Apollo Engine
         persisting: false, // Optional
-        // Use websockets for everything (no HTTP)
-        // You need to pass a `wsEndpoint` for this to work
         websocketsOnly: false // Optional
       }
-      // test: {
-      //   httpEndpoint: 'http://localhost:5000',
-      //   wsEndpoint: 'ws://localhost:5000',
-      //   tokenName: 'apollo-token'
-      // },
-      // alternative: user path to config which returns exact same config options
-      // test2: '~/plugins/my-alternative-apollo-config.js'
     }
   },
   /*
---
+++ www/store/index.js
@@ -0,0 +1,16 @@
+// export const state = () => ({
+//   userEmail: ''
+// })
+
+// export const mutations = {
+//   setUserEmail (state, userEmail) {
+//     state.userEmail = userEmail
+//   }
+// }
+
+// export const actions = {
+//   setUserEmail ({ commit }) {
+//     const userEmail = 'asdf'
+//     commit('setUserEmail', userEmail)
+//   }
+// }
